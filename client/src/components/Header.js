0aa6a8b61a521a324cef1b5ce746fba33fc6c298
pajelonek <pawel.jelonek.krakow@gmail.com>
2021-01-29 19:11:48 +0100

    Displaying response from backend.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Album() {
+export default function Header() {
     return (
         <h1>TwitchClipsViewer</h1>
     );
--- client/src/components/MultiContainer.js
+++ client/src/components/MultiContainer.js
@@ -1,13 +1,7 @@
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import CardActions from "@material-ui/core/CardActions";
-import Button from "@material-ui/core/Button";
+import React, {Component} from "react";
 import Container from "@material-ui/core/Container";
-import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import ClipsGrid from './ClipsGrid';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -28,43 +22,31 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+class MultiContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clipResponse: []
+        }
+    }
 
-export default function MultiContainer() {
-    const classes = useStyles();
+    componentDidMount() {
+        fetch("http://localhost:3001/api/v1/getClips")
+            .then(response => response.json())
+            .then(response => this.setState({
+                clipResponse: response
+            }));
+    }
 
-    return (
-        <Container className={classes.cardGrid} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
-                {cards.map((card) => (
-                    <Grid item key={card} xs={12} sm={6} md={4}>
-                        <Card className={classes.card}>
-                            <CardMedia
-                                className={classes.cardMedia}
-                                image="https://source.unsplash.com/random"
-                                title="Image title"
-                            />
-                            <CardContent className={classes.cardContent}>
-                                <Typography gutterBottom variant="h5" component="h2">
-                                    Heading
-                                </Typography>
-                                <Typography>
-                                    This is a media card. You can use this section to describe the content.
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    View
-                                </Button>
-                                <Button size="small" color="primary">
-                                    Edit
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
-    );
+    render() {
+        if (!this.state.clipResponse)
+            return <Container className={useStyles.cardGrid} maxWidth="md"/>;
+        else {
+            return <Container className={useStyles.cardGrid} maxWidth="md">
+                <ClipsGrid props={this.state.clipResponse}/>
+            </Container>;
+        }
+    }
 }
+
+export default MultiContainer;
