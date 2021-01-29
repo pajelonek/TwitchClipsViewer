cf6fab995f649421fe1713e89f507ab726b7be46
pajelonek <pawel.jelonek.krakow@gmail.com>
2021-01-29 19:05:34 +0100

    Fixing sample response

--- app/controllers/api/v1/clips_controller.rb
+++ app/controllers/api/v1/clips_controller.rb
@@ -5,13 +5,13 @@ require 'json'
 module Api::V1
   class ClipsController < ApplicationController
     def get_clips
-      url = 'https://api.twitch.tv/kraken/clips/top?channel=Twitch&period=month&trending=true&limit=1'
+      url = 'https://api.twitch.tv/kraken/clips/top?period=month&trending=true&limit=9'
       headers = {
         "Accept"  => "application/vnd.twitchtv.v5+json",
         "Client-ID" => "k0p7lgsthuzos2rbeh8cry5zdx4uew"
       }
       response = HTTParty.get(url, :headers => headers)
-      render json: JSON.parse(response.body)['clips']
+      render json: JSON.parse(response.body)
     end
   end
 end
