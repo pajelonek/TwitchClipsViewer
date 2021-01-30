737361d45934b90b6ab766ba109d6a727247e7ae
pajelonek <pawel.jelonek.krakow@gmail.com>
2021-01-30 13:25:04 +0100

    Adding simple menuBar

--- client/src/components/ClipsGrid.js
+++ client/src/components/ClipsGrid.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import Grid from "@material-ui/core/Grid";
 import {makeStyles} from "@material-ui/core/styles";
-import {Card, CardActions} from "@material-ui/core";
+import {CardActions} from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import CardMedia from "@material-ui/core/CardMedia";
+import CardActionArea from "@material-ui/core/CardActionArea";
 
 const useStyles = makeStyles((theme) => ({
     cardGrid: {
@@ -33,7 +34,7 @@ class ClipsGrid extends Component {
             return <Grid container spacing={4}>
                 {this.props.props.clips.map((clip) => (
                     <Grid item key={clip.tracking_id} xs={12} sm={6} md={4}>
-                        <Card className={useStyles.card}>
+                        <CardActionArea className={useStyles.card}>
                             <CardMedia style={{height: 0, paddingTop: '56%'}}
                                        className={useStyles.cardMedia}
                                        image={clip.thumbnails.medium}
@@ -55,7 +56,7 @@ class ClipsGrid extends Component {
                                     Edit
                                 </Button>
                             </CardActions>
-                        </Card>
+                        </CardActionArea>
                     </Grid>
                 ))}
             </Grid>;
