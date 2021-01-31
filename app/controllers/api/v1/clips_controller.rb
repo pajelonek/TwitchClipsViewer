9fdbc794f76713cf0916fca18b2ad9a1bfc4a42d
pajelonek <pawel.jelonek.krakow@gmail.com>
2021-01-31 15:20:15 +0100

    Making rails able to handle different responses.

--- app/controllers/api/v1/clips_controller.rb
+++ app/controllers/api/v1/clips_controller.rb
@@ -1,13 +1,20 @@
 require 'httparty'
 require 'json'
 
-
 module Api::V1
   class ClipsController < ApplicationController
+    @@period = 'month'
+    @@trending = 'true'
+    @@limit = '9'
+    @@base_url = 'https://api.twitch.tv/kraken/clips/top?'
+    @@headers = {
+      "Accept" => "application/vnd.twitchtv.v5+json",
+      "Client-ID" => "k0p7lgsthuzos2rbeh8cry5zdx4uew"
+    }
     def get_clips
       url = 'https://api.twitch.tv/kraken/clips/top?period=month&trending=true&limit=9'
       headers = {
-        "Accept"  => "application/vnd.twitchtv.v5+json",
+        "Accept" => "application/vnd.twitchtv.v5+json",
         "Client-ID" => "k0p7lgsthuzos2rbeh8cry5zdx4uew"
       }
       response = HTTParty.get(url, :headers => headers)
@@ -15,13 +22,18 @@ module Api::V1
     end
 
     def post_clips
-      if JSON.parse(request.body.read)['test'] == nil
+      if request['limit'] == nil or request['period'] == nil
         e = Errors::StandardError.new
         render json: Errors::ErrorSerializer.new(e), status: e.status
-      else render text:"Hello #{request.body.read}"
+      else
+        if request['trending'] != nil
+          @@trending = request['trending']
+        end
+        url = @@base_url+'period='+request['period']+'&trending='+@@trending+'&limit='+request['limit']
+        response = HTTParty.get(url, :headers => @@headers)
+        render json: JSON.parse(response.body)
       end
     end
-
 
   end
 end
